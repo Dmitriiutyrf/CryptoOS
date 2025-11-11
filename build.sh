6d44c78c2cddda847db6333bedbf38a863f6a2ed
ooo.solomon <ooo.solomon@bk.ru>
2025-11-11 04:19:53 +0000

    Implement manual dependency installation with build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Print commands and their arguments as they are executed.
+set -x
+
+# Install Python dependencies
 pip install -r requirements.txt
